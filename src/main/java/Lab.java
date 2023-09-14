c77c1fb78fb49430e1931b0c6997fbaebb8075b7
ted balashov <theodore.balashov@revature.com>
2023-09-14 14:45:14 -0400

    lab

---
+++ src/main/java/Lab.java
@@ -0,0 +1,10 @@
+public class Lab {
+    /**
+     * convert seconds to HHMMSS format. eg, 1432 = 00:23:52
+     * @param seconds an int representation of seconds.
+     * @return seconds in HHMMSS format.
+     */
+    public String convert(int seconds){
+        return null;
+    }
+}
